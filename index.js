67ec8437b1ffc1e86b6c432237a9e7739bfa7d3c
Tominous <18189490+Tominous@users.noreply.github.com>
2019-10-09 16:27:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function notifyDiscord(imageUrl, payload, location, action) {
 		{ json: data },
 		function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				//console.log(body)
+				console.log(body)
 			}
 		}
 	);
